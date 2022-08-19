ac30bdd9aeea3ac48b7ad181de8a9824fc53d17c
waonpad <waonpad@gmail.com>
2022-08-19 23:28:11 +0900

    フォロー機能

--- src/resources/js/pages/User.tsx
+++ src/resources/js/pages/User.tsx
@@ -12,39 +12,34 @@ function User(): React.ReactElement {
     // 表示中のユーザーのid
     const {id} = useParams<{id: string}>();
 
-    // 自分のid
-    const [myid, setMyId] = useState('');
-
     const [FollowButton, setFollowButton] = useState(<ReactLoading type="spin" height="20px" width="20px" />);
 
-    if (!localStorage.getItem('auth_token')){
-        setMyId('');
+    const follow = () => {
+        axios.post('/api/follow', {screen_name: id}).then(res => {
+            if(res.data.status === true) {
+                console.log(res);
+                
+                setFollowButton((
+                    <React.Fragment>
+                        <Button onClick={unfollow}>UnFollow</Button>
+                    </React.Fragment>
+                ))
+            }
+        })
     }
-    else {
-        useEffect(() => {
-            axios.get(`/api/myself`).then(res => {
-                if (res.status === 200) {
-                    setMyId(res.data.screen_name);
-                    console.log(res);
 
-                    // 表示中のユーザーが自分であるか
-                    if(id === res.data.screen_name) {
-                        setFollowButton((
-                            <React.Fragment>
-                                <Button>Edit Profile</Button>
-                            </React.Fragment>
-                        ))
-                    }
-                    else {
-                        setFollowButton((
-                            <React.Fragment>
-                                <Button>Follow</Button>
-                            </React.Fragment>
-                        ))
-                    }
-                }
-            });
-        }, [])
+    const unfollow = () => {
+        axios.post('/api/unfollow', {screen_name: id}).then(res => {
+            if(res.data.status === true) {
+                console.log(res);
+                
+                setFollowButton((
+                    <React.Fragment>
+                        <Button onClick={follow}>Follow</Button>
+                    </React.Fragment>
+                ))
+            }
+        })
     }
 
     const [userData, setUserData] = useState({
@@ -52,6 +47,7 @@ function User(): React.ReactElement {
         name: '',
         email: '',
         password: '',
+        status: true,
     });
 
     const data = {
@@ -65,12 +61,49 @@ function User(): React.ReactElement {
                 console.log(res);
             }
         });
+        
+        if (!localStorage.getItem('auth_token')){
+            setFollowButton((
+                <React.Fragment>
+                    <Button onClick={follow}>Follow</Button>
+                </React.Fragment>
+            ))
+        }
+        else {
+            axios.get('/api/ffcheck', {params: {screen_name: id}}).then(res => {
+                if(res.status === 200) {
+                    console.log(res);
+                    
+                    // 表示中のユーザーが自分であるか フォローしているかしていないか
+                    if(res.data.myself === true) {
+                        setFollowButton((
+                            <React.Fragment>
+                                <Button>Edit Profile</Button>
+                            </React.Fragment>
+                        ))
+                    }
+                    else if(res.data.follow === true) {
+                        setFollowButton((
+                            <React.Fragment>
+                                <Button onClick={unfollow}>UnFollow</Button>
+                            </React.Fragment>
+                        ))
+                    }
+                    else if(res.data.follow === false) {
+                        setFollowButton((
+                            <React.Fragment>
+                                <Button onClick={follow}>Follow</Button>
+                            </React.Fragment>
+                        ))
+                    }
+                }
+            })
+        }
     }, [])
 
     return (
         <React.Fragment>
             <h1>User:{id}</h1>
-            {/* タスク：フォロー機能を作る */}
             {FollowButton}<br />
             {userData.screen_name ? <span>{userData.screen_name}</span> : <ReactLoading type="spin" height="20px" width="20px" />}<br />
             {userData.name ? <span>{userData.name}</span> : <ReactLoading type="spin" height="20px" width="20px" />}<br />
