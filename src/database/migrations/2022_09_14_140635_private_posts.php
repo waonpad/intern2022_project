00d95da862b0b1986d12c113f88a8695f0a988f6
waonpad <waonpad@gmail.com>
2022-09-15 23:14:32 +0900

    微修正

--- src/database/migrations/2022_09_14_140635_private_posts.php
+++ src/database/migrations/2022_09_14_140635_private_posts.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('private_posts', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->unsignedBigInteger('send_user_id');
             $table->unsignedBigInteger('receive_user_id');
             $table->foreign('send_user_id')->references('id')->on('users')->onDelete('cascade');
--- src/resources/js/pages/PrivateChat.tsx
+++ src/resources/js/pages/PrivateChat.tsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import swal from "sweetalert";
 import ReactDOM from 'react-dom';
 import { Button, Card } from '@material-ui/core';
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useHistory } from "react-router-dom";
 import axios from 'axios';
 import { useForm, SubmitHandler } from "react-hook-form";
 import TextField from '@mui/material/TextField';
 import { LoadingButton } from '@mui/lab';
+import ReactLoading from 'react-loading';
 
 import {useAuth} from "../AuthContext";
 
@@ -25,11 +26,13 @@ declare global {
 function PrivateChat(): React.ReactElement {
 
     const auth = useAuth();
-
+    const history = useHistory();
+    
     const {id} = useParams<{id: string}>();
     const [disp_user_id, setDispUserId] = useState(Number);
 
     const { register, handleSubmit, setError, clearErrors, formState: { errors } } = useForm<PrivatePostData>();
+    const [initial_load, setInitialLoad] = useState(true);
     const [loading, setLoading] = useState(false);
 
     const onSubmit: SubmitHandler<PrivatePostData> = (data: PrivatePostData) => {
@@ -65,11 +68,12 @@ function PrivateChat(): React.ReactElement {
         },     
     ]
 
-    const [privateposts, setPrivatePosts] = useState(initialState);
+    const [private_posts, setPrivatePosts] = useState(initialState);
 
 	useEffect(() => {
+
         const data = {
-            screen_name: id,
+            screen_name: id
         };
 
         axios.get('/api/getuser', {params: data}).then(res => {
@@ -81,54 +85,69 @@ function PrivateChat(): React.ReactElement {
 
                 console.log(channelname);
 
-                window.Echo.private('privatepost.' + channelname).listen('PrivatePosted', (e: any) => {
+                window.Echo.private('private_post.' + channelname).listen('PrivatePosted', (e: any) => {
                     console.log(e);
                     console.log(e.private_post.text);
-                    setPrivatePosts(privateposts => [...privateposts,{text: e.private_post.text}]);
+                    setPrivatePosts(private_posts => [...private_posts,{text: e.private_post.text}]);
                 });
+
+                setInitialLoad(false);
             }
+        }).catch((error) => {
+            // console.log(error);
+            swal("ユーザーがいない", "そのユーザーは存在しません", "error");
+            history.push('/');
+            // setInitialLoad(false);
         });
 	}, [])
 
-    return (
-        <React.Fragment>
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">
-                                <ul id='board'>
-                                    { privateposts.map((privatepost, index) => (
-                                    <li key={ index }>{ privatepost.text }</li>
-                                    ))}
-                                </ul>
-                            </div>
-
-                            <form className="py-4" onSubmit={e => {clearErrors(); handleSubmit(onSubmit)(e)}}>
-                                <div className="card-body">
-                                    <div className="py-4">
-                                        <TextField
-                                            fullWidth
-                                            variant="outlined" 
-                                            id="text"
-                                            label="メッセージ" 
-                                            {...register('text', {
-                                            required: '入力してください'
-                                            })} 
-                                        />
-                                    </div>
-                                        
-                                    <div>
-                                        <LoadingButton loading={loading} type="submit" variant="contained" fullWidth>Submit</LoadingButton>
-                                    </div>
+    
+	if (initial_load) {
+		return (
+			<ReactLoading type="spin" height="100px" width="100px" />
+		)
+	}
+	else {
+        return (
+            <React.Fragment>
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-md-8">
+                            <div className="card">
+                                <div className="card-header">
+                                    <ul id='board'>
+                                        { private_posts.map((private_post, index) => (
+                                        <li key={ index }>{ private_post.text }</li>
+                                        ))}
+                                    </ul>
                                 </div>
-                            </form>
+    
+                                <form className="py-4" onSubmit={e => {clearErrors(); handleSubmit(onSubmit)(e)}}>
+                                    <div className="card-body">
+                                        <div className="py-4">
+                                            <TextField
+                                                fullWidth
+                                                variant="outlined" 
+                                                id="text"
+                                                label="メッセージ" 
+                                                {...register('text', {
+                                                required: '入力してください'
+                                                })} 
+                                            />
+                                        </div>
+                                            
+                                        <div>
+                                            <LoadingButton loading={loading} type="submit" variant="contained" fullWidth>Submit</LoadingButton>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </React.Fragment>
-    );
+            </React.Fragment>
+        );
+	}
 }
 
 export default PrivateChat;
